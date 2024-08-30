b3815be7d5d459a68f7a38983cfb2ff027cea57c
mamartam <marta.masiukk@gmail.com>
2024-08-30 10:47:44 +0300

    reated new folder - mouseover&out; did firdt task in which I changes color of block

---
+++ mouseOver&Out/task1/script.js
@@ -0,0 +1,7 @@
+const divv = document.getElementById("divv");
+divv.addEventListener("mouseover", function(){
+    divv.style.background = "red";
+})
+divv.addEventListener("mouseout", function(){
+    divv.style.background = "green";
+})
