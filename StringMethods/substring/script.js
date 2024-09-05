cdaa180b123288fd3b159db3db251eacd4802456
mamartam <marta.masiukk@gmail.com>
2024-09-05 10:55:22 +0300

    id task with substring

---
+++ StringMethods/substring/script.js
@@ -0,0 +1,7 @@
+// Task 1
+let string1 = 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias, tempora.';
+function newStringg(str) {
+    let result = str.substring(2, 6);
+    console.log(result);
+}
+newStringg(string1);
