9cf6e048267e1aac0d678d650801a804578ffe85
mamartam <marta.masiukk@gmail.com>
2024-09-06 11:24:07 +0300

    created folder or task related to array

---
+++ Array/push/script.js
@@ -0,0 +1,23 @@
+// Task 1
+const array1 = ["Ukraine", "Canada", "Germany", "Japan", "Australia"];
+console.log(array1);
+array1.push("USA");
+array1.push("Switzerland");
+console.log(array1);
+
+// Task 2
+const fruits = ["Apple", "Orange", "Banana"];
+console.log(fruits);
+fruits.push("Pear");
+console.log(fruits);
+console.log("There are " + fruits.length + " types of fruits");
+
+// Task 3
+const books = ['1984', 'Broken toys', 'Pigmalion'];
+console.log(books);
+books.push("Don't look back and be silend");
+console.log("There are " + books.length + " books");
+console.log("First book is " + books[0]);
+console.log("The last book is " + books[books.length-1]);
+
+
